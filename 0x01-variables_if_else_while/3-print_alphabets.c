e7aba33f7d26d429f736f637f5f68adce50a5ad8
richbeauty1225 <richbeauty25@gmail.com>
2023-03-20 19:08:19 -0700

    Task three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main -Entry Point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int lowerC, upperC;
+
+        lowerC = 'a';
+        upperC = 'A';
+
+        while (lowerC <= 'z')
+        {
+                putchar(lowerC);
+                lowerC += 1;
+        }
+        while (upperC <= 'Z')
+        {
+                putchar(upperC);
+                upperC += 1;
+        }
+        putchar('\n');
+        return (0);
+}
