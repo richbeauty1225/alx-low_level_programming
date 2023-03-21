f2c04432e759a023e23ee28650991f5da4613236
richbeauty1225 <richbeauty25@gmail.com>
2023-03-20 18:14:26 -0700

    Updated task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,12 +15,9 @@
 int main(void)
 {
 int n, last_digit;
-
-  srand(time(0));
-    n = rand() - RAND_MAX / 2;
-
-    last_digit = (n % 10);
-
+ srand(time(0));
+ n = rand() - RAND_MAX / 2;
+ last_digit = (n % 10);
 if (last_digit > 5)
 {
 :printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
