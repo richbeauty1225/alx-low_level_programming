38b7450f9d814a458c6ab0c231e4dca3ca06aa01
richbeauty1225 <richbeauty25@gmail.com>
2023-03-20 18:00:07 -0700

    Task one

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Programme that shows the last dgit of n
+ *if n is greater than 5 show is greater than 5
+ *if n is 0 show n is zero
+ *if n is less than 6 show n is less than six and not 0
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int n, last_digit;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        last_digit = (n % 10);
+
+if (last_digit > 5)
+{
+   printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+}
+else if (last_digit == 0)
+{
+   printf("Last digit of %d is %d and is 0\n", n, last_digit);
+}
+else if (last_digit < 6 && last_digit != 0)
+{
+    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+}
+return (0);
+}
