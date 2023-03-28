f1bef40bb9f1dd653b99e9af35655101b405b78b
richbeauty1225 <richbeauty25@gmail.com>
2023-03-28 11:19:34 -0700

    C pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**                                                                                                   * swap_int - take in two varibale integers and swaps them
+ * @author Anisah
+ * @a: swaps and stores address of b
+ * @b: swaps and stores address of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int change;
+change = *b;
+*b = *a;
+*a = change;
+}
