be007f67ce5a677e089977c6921bd2eb347b4986
richbeauty1225 <richbeauty25@gmail.com>
2023-03-30 14:33:43 -0700

    Pointer

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,15 @@
 char *string_toupper(char *s)
 {
 
-        int i;
+	int i;
 
 i = 0;
-        while (*(s + i))
-        {
-                if (*(s + i) >= 'a' && *(s + i) <= 'z')
-                        *(s + i) -= 'a' - 'A';
-                i++;
-        }
-        return (s);
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
 }
 /*Chemist*/
